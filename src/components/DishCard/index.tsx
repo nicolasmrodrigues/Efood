81d0df30db1157a457b6a4b7c2e565fea63353ea
Nícolas Medeiros Rodrigues <nicolasmedeiros090@gmail.com>
2024-04-18 13:15:27 -0300

    feat: add dish modal

--- src/components/DishCard/index.tsx
+++ src/components/DishCard/index.tsx
@@ -1,18 +1,57 @@
 import formatDescription from '../../utils/FormatDescription'
 import * as S from './styles'
 import { DishType } from '../../pages/RestaurantProfile'
+import { Container } from '../../styles'
+import close from '../../assets/images/close.png'
+import { useState } from 'react'
 
 export type Props = {
   dish: DishType
 }
 
-const DishCard = ({ dish }: Props) => (
-  <S.Card>
-    <S.Image src={dish.foto} alt="" />
-    <S.Title>{dish.nome}</S.Title>
-    <S.Description>{formatDescription(dish.descricao, 165)}</S.Description>
-    <S.Button>Adicionar ao carrinho</S.Button>
-  </S.Card>
-)
+const DishCard = ({ dish }: Props) => {
+  const [isVisible, setIsvisible] = useState(false)
+
+  const formatPrice = (price: number) => {
+    return new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(price)
+  }
+
+  return (
+    <>
+      <S.Card>
+        <S.Image src={dish.foto} alt="" />
+        <S.Title>{dish.nome}</S.Title>
+        <S.Description>{formatDescription(dish.descricao, 165)}</S.Description>
+        <S.Button onClick={() => setIsvisible(true)}>
+          Adicionar ao carrinho
+        </S.Button>
+      </S.Card>
+      <S.Modal className={isVisible ? 'visible' : ''}>
+        <Container>
+          <S.ModalContent>
+            <div>
+              <S.ModalImage src={dish.foto} alt={`Foto de ${dish.nome}`} />
+            </div>
+            <div>
+              <S.TitleBar>
+                <S.ModalTitle>{dish.nome}</S.ModalTitle>
+                <img onClick={() => setIsvisible(false)} src={close} alt="" />
+              </S.TitleBar>
+              <p>{dish.descricao}</p>
+              <span>{`Serve: de ${dish.porcao}`}</span>
+              <S.ModalButton>{`Adicionar ao carrinho - ${formatPrice(
+                dish.preco
+              )}`}</S.ModalButton>
+            </div>
+          </S.ModalContent>
+        </Container>
+        <S.Overlay onClick={() => setIsvisible(false)} />
+      </S.Modal>
+    </>
+  )
+}
 
 export default DishCard
--- src/components/DishCard/styles.ts
+++ src/components/DishCard/styles.ts
@@ -46,3 +46,82 @@ export const Button = styled.button`
     background-color: #eddccc;
   }
 `
+
+export const Modal = styled.div`
+  display: none;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  color: #fff;
+  font-size: 14px;
+  line-height: 22px;
+  z-index: 1;
+  align-items: center;
+}
+
+  &.visible {
+    display: flex;
+  }
+
+  p {
+    margin-bottom: 24px;
+  }
+`
+
+export const ModalContent = styled.div`
+  display: flex;
+  background-color: ${colors.pink};
+  padding: 32px;
+  position: relative;
+  z-index: 1;
+
+  span {
+    display: block;
+  }
+`
+
+export const ModalImage = styled.img`
+  width: 280px;
+  height: 280px;
+  object-fit: cover;
+  margin-right: 24px;
+`
+
+export const ModalTitle = styled.h4`
+  font-size: 18px;
+  font-weight: 900;
+  color: #fff;
+  margin-bottom: 16px;
+`
+
+export const TitleBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  img {
+    width: 16px;
+    height: 16px;
+    position: absolute;
+    right: 8px;
+    top: 8px;
+    cursor: pointer;
+  }
+`
+
+export const Overlay = styled.div`
+  background-color: rgba(0, 0, 0, 0.8);
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`
+
+export const ModalButton = styled(Button)`
+  width: auto;
+  text-align: center;
+  padding: 4px 8px;
+  margin-top: 16px;
+`
