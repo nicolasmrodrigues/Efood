b8f9f18caede5d7df315606ff4fe5161738af37c
Nícolas Medeiros Rodrigues <nicolasmedeiros090@gmail.com>
2024-04-23 14:59:31 -0300

    feat: add cart reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { Provider } from 'react-redux'
+import Cart from './components/Cart'
 import Footer from './components/Footer'
 import RoutesElement from './routes'
 import GlobalStyle from './styles'
 import { BrowserRouter } from 'react-router-dom'
+import store from './store'
 
 function App() {
   return (
     <BrowserRouter>
-      <GlobalStyle />
-      <RoutesElement />
-      <Footer />
+      <Provider store={store}>
+        <GlobalStyle />
+        <RoutesElement />
+        <Footer />
+      </Provider>
     </BrowserRouter>
   )
 }
